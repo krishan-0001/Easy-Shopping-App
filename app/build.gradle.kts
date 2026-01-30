f51f1a4f2b94775f980efbdc94595be71c89f713
krishan-0001 <ks8464664@gmail.com>
2026-01-31 00:08:18 +0530

    Header view and Banner View

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 
 dependencies {
 
-    
+    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -62,4 +62,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation ("io.coil-kt:coil-compose:2.7.0")
+    implementation("com.tbuonomo:dotsindicator:5.1.0")
 }
